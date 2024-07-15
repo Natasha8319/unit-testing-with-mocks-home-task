f402ef0959156d2d38f1a4d2adc085537c4362d0
Natasha Mishuk <Natasha_Mishuk@epam.com>
2024-07-15 10:02:55 +0200

    test commit for jenkins

--- src/tests/data_handlers/user_data_handler.test.js
+++ src/tests/data_handlers/user_data_handler.test.js
@@ -101,7 +101,7 @@ describe('user_data_handler test suite', () => {
         testDescription: 'searchTestData.invalidPhone',
       },
       // {
-      //   new test raw3
+      //   new test raw4
       //   user: searchTestData.testUser,
       //   searchParams: searchTestData.validAddress,
       //   expectedResult: true,
